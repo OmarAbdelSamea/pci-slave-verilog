c0d573f7e324835affabffd789d88afa7af76b45
Loayelshall <Loay.elshall@icloud.com>
2020-12-22 13:13:02 +0200

    devSelect final test

--- devSelect.v
+++ devSelect.v
@@ -1,19 +1,22 @@
 module devSelect(clk, frame,decoderInput, devSelect);
 
 input wire clk, frame, decoderInput;
-output reg devSelect = 0;
+output reg devSelect = 1;
 reg delayReg =0;
 reg delayReg2=0 ;
 
 always@(negedge clk)
 begin 
-if(frame == 0)
+if(negedge frame)
 begin
     delayReg <= decoderInput;
     delayReg2 <= delayReg;
     devSelect <= delayReg2; 
-
-
+end
+else if(posedge frame)
+begin 
+    delayReg <= 1;
+    devSelect <= delayReg;
 end
 end
 
@@ -27,19 +30,29 @@ reg clk, frame, decoderInput;
 wire devSelect ;
 initial 
 begin
+clk = 0;
 $monitor($time ,,,"Devsel=%d frame=%d decoderInput=%d clk=%d",devSelect,frame ,decoderInput,clk);
+    
+
+    frame = 1;
+    decoderInput = 0;
+
+    #10
     frame = 0;
-    clk = 1;
     decoderInput = 1;
-    #25
-    clk = 0;
 
-    #25 clk =1;
-    #25 clk =0;
+    #30
+    decoderInput = 0;
 
-    #25 clk =1;
-    #25 clk =0;
+    #200
+    frame = 0;
 
+end
+
+
+always 
+begin
+     #10 clk=~clk;
 end
 devSelect d1(clk,frame,decoderInput,devSelect);
 
