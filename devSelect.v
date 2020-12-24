e2423dc0c7a94fa0b75ca56a93ab5d31579e9bbe
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2020-12-24 20:01:16 +0200

    finished devselect

--- devSelect.v
+++ devSelect.v
@@ -2,60 +2,57 @@
 module devSelect(clk, frame,decoderInput, devSelect);
 
 input wire clk, frame;
-input wire [2:0]decoderInput;
-output reg devSelect = 1;
-reg delayReg =1;
+input wire [1:0] decoderInput;
+output reg devSelect;
+reg delayReg=1 ;
 reg delayReg2=1;
-reg onChange =1;
-
+reg onChange=1;
+ 
 always@(posedge clk)
 begin 
-if (decoderInput > -1 && decoderInput < 4)
+if (frame == 0)
 begin
-if(!frame)
-begin
-    delayReg <= 0;
-    delayReg2 <= delayReg;
-    onChange <= delayReg2; 
-end
-end
-if(frame)
-begin 
-    delayReg <= 1;
-    onChange <= delayReg;
-end
-end
+ if(decoderInput < 3)
+ 	begin
+    	delayReg <= frame;
+    	onChange <= delayReg; 
+	end
 
+end
+else
+begin 
+  onChange<=1;
+end
+end
 always@(negedge clk)
 begin
     devSelect <= onChange;
-
 end
-
-
 endmodule
 
 
 module devSelectTB();
 
 reg clk, frame;
-reg [2:0]decoderInput;
+reg [1:0]decoderInput;
 wire devSelect ;
 initial 
 begin
-clk = 0;
-$monitor($time ,,,"Devsel=%d frame=%d decoderInput=%d clk=%d",onChange,frame ,decoderInput,clk);
+
+$monitor($time ,,,"Devsel=%d frame=%d decoderInput=%d clk=%d",devSelect,frame ,decoderInput,clk);
     
-
+    clk = 0;
     frame = 1;
-    decoderInput = 6;
-
-#10
-decoderInput = 1;
+    decoderInput = 1;
     #20
     frame = 0;
-    
-
+    decoderInput = 1;
+    #50
+    decoderInput = 3;
+    frame =0;
+    #50
+    decoderInput = 5;
+    frame =0;
     #50
     decoderInput = 7;
 
@@ -69,6 +66,6 @@ always
 begin
      #10 clk=~clk;
 end
-devSelect d1(clk,frame,decoderInput,devSelect);
+devSelect d1(clk, frame,decoderInput, devSelect);
 
 endmodule
